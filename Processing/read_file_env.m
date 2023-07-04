49656ae6727972ef2dfafb1f22c7eb0757db6ba2
Michiel <michielvannesselrooij@gmail.com>
2023-07-04 09:49:00 +0200

    Also separate measurements by changes in rpm
    In case a row of zeros is missing

--- Processing/read_file_env.m
+++ Processing/read_file_env.m
@@ -8,8 +8,16 @@ function [T, Troom, pa, hum, tunnel_q, tunnel_T, tunnel_pa,...
 
     data = importdata(fileName);
     
-    % Separate measurements via empty rows
-    filter = [0; find(sum(abs(data),2) == 0)];
+    % Separate measurements 
+    % A) via empty rows
+    filter1 = [1; find(sum(abs(data),2) == 0)+1; length(data)+1];
+
+    % B) via steps in rpm
+    rpm = data(:,7);
+    rpmChange = rpm(2:end) - rpm(1:end-1);
+    filter2 = find(rpmChange > 5)+1;
+
+    filter = sort(unique( [filter1; filter2] ));
     
     % Initialize
     T           = zeros(length(filter)-1,1);
@@ -29,20 +37,20 @@ function [T, Troom, pa, hum, tunnel_q, tunnel_T, tunnel_pa,...
     for i = 1:length(filter)-1
         
         % Import Hill sensor data
-        T(j)     = mean(data( filter(i)+1 : filter(i+1)-1, 4));              % Temperature test section [C]
-        Troom(j) = mean(data( filter(i)+1 : filter(i+1)-1, 5));              % Temperature at control box [C]
-        pa(j)    = mean(data( filter(i)+1 : filter(i+1)-1, 6));              % Absolute pressure [Pa]
-        hum(j)   = mean(data( filter(i)+1 : filter(i+1)-1, 3));              % Relative humidity [%]
-        q(j)     = mean(data( filter(i)+1 : filter(i+1)-1, 7));              % Mensor (TEMP)
+        T(j)     = mean(data( filter(i) : filter(i+1)-1, 4));              % Temperature test section [C]
+        Troom(j) = mean(data( filter(i) : filter(i+1)-1, 5));              % Temperature at control box [C]
+        pa(j)    = mean(data( filter(i) : filter(i+1)-1, 6));              % Absolute pressure [Pa]
+        hum(j)   = mean(data( filter(i) : filter(i+1)-1, 3));              % Relative humidity [%]
+        q(j)     = mean(data( filter(i) : filter(i+1)-1, 7));              % Mensor (TEMP)
         
         
         % Optionally import tunnel sensor data
         if strcmp(tunnel,'M-tunnel')
         
-            tunnel_q(j)   = mean(data( filter(i)+1 : filter(i+1)-1, 7));     % Dynamic pressure (q)
-            tunnel_T(j)   = mean(data( filter(i)+1 : filter(i+1)-1, 8));     % Temperature test section [C]
-            tunnel_pa(j)  = mean(data( filter(i)+1 : filter(i+1)-1, 9))*100; % Absolute pressure [Pa]
-            tunnel_rpm(j) = mean(data( filter(i)+1 : filter(i+1)-1, 10));    % Fan rpm
+            tunnel_q(j)   = mean(data( filter(i) : filter(i+1)-1, 7));     % Dynamic pressure (q)
+            tunnel_T(j)   = mean(data( filter(i) : filter(i+1)-1, 8));     % Temperature test section [C]
+            tunnel_pa(j)  = mean(data( filter(i) : filter(i+1)-1, 9))*100; % Absolute pressure [Pa]
+            tunnel_rpm(j) = mean(data( filter(i) : filter(i+1)-1, 10));    % Fan rpm
             tunnel_hum    = hum(j);                                          % Not available. Use Hill data.
         
         % elseif strcmp(tunnel,'W-tunnel')
@@ -65,7 +73,6 @@ function [T, Troom, pa, hum, tunnel_q, tunnel_T, tunnel_pa,...
         
     end
 
-    % Tell what I've done
     disp(['Succesfully imported file ' fileName]);
     disp(' ');
 
